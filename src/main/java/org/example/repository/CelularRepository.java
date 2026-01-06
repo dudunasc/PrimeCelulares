4b03ee6c5c96e5bb6fb45c424efd12888d054e26
Luiz Eduardo <eduardonoliveira13@gmail.com>
2026-01-05 22:40:47 -0300

    Iniciando projeto

---
+++ src/main/java/org/example/repository/CelularRepository.java
@@ -0,0 +1,12 @@
+package org.example.repository;
+
+import org.example.entities.Celular;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CelularRepository {
+    private List<Celular> celulares = new ArrayList<>();
+
+
+}
